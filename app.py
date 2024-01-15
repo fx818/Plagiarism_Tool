8fb358463926ba92cc5dab5dc972d884d9b54915
Anurag Upadhyay <anuragfx818@gmail.com>
2024-01-15 09:10:09 +0530

    changes

--- app.py
+++ app.py
@@ -7,11 +7,13 @@ st.title('Plagiarism Tool')
 
 x = st.text_input('Enter sentence 1 here', placeholder='Sample Text')
 y = st.text_input('Enter sentence 2 here', placeholder='sample text')
-
-if len(x)==0 or len(y)==0:
+if len(x)==0:
     x = 'Sample Text'
     y = 'sample text'
-
+if len(y)==0:
+    x = 'Sample Text'
+    y = 'sample text'
+    
 z = tool(x,y)[0]
 m = tool(x,y)[1]
 section = st.sidebar.radio('Explore Different Tools from here :' , ['Plagiarism Tool','Bag of Word Model','Chart Analysis'])
